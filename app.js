226dd3cbca0dc054d322b1feebbd1d8d61d13bd9
Noah Krull <153031922+whirr1@users.noreply.github.com>
2025-07-01 15:16:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-// This component will be the main layout for ALL your pages
 const Layout = ({ children }) => {
     return (
-        // The empty fragment <>...</> is needed because a component must return a single element
         <>
             <nav>
                 <ul>
-                    {/* These now point to separate HTML files */}
                     <li><a href="/index.html">Home</a></li>
                     <li><a href="/education.html">Education</a></li>
                     <li><a href="/accomplishments.html">Accomplishments</a></li>
@@ -19,7 +16,6 @@ const Layout = ({ children }) => {
                 <p>Digital Resume and Blog</p>
             </header>
 
-            {/* 'children' is where the unique content of each page will go */}
             <main>
                 {children}
             </main>
@@ -31,14 +27,12 @@ const Layout = ({ children }) => {
     );
 };
 
-// --- Content for the HOME PAGE (index.html) ---
 const HomePage = () => {
     return (
         <Layout>
             <h2>Welcome!</h2>
             <p>This is the main page of my website. It's great to have you here!</p>
             <p>Feel free to look around at my accomplishments, projects, and my personal blog.</p>
-            {/* Add any other basic info you want on the home page here */}
         </Layout>
     );
 };
