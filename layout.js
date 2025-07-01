6c61171aaecce50ee4c651b52ce9199171900ee9
Noah Krull <153031922+whirr1@users.noreply.github.com>
2025-07-01 15:19:47 -0500

    Create layout.js

---
+++ layout.js
@@ -0,0 +1,27 @@
+// This is our single, shared Layout component.
+const Layout = ({ children }) => {
+    return (
+        <>
+            <nav>
+                <ul>
+                    {/* Using relative links is the safest option */}
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="education.html">Education</a></li>
+                    <li><a href="accomplishments.html">Accomplishments</a></li>
+                    <li><a href="blog.html">Blog</a></li>
+                    <li><a href="projects.html">Projects</a></li>
+                </ul>
+            </nav>
+            <header>
+                <h1>Noah Krull's Awesome Website!</h1>
+                <p>Digital Resume and Blog</p>
+            </header>
+            <main>
+                {children}
+            </main>
+            <footer>
+                <p>&copy; 2025 Noah Krull</p>
+            </footer>
+        </>
+    );
+};
